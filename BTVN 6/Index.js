23605e4d0fdb6e07a3111dcb7ea537473b06c3b1
ZKlegend <lekinhanh91@gmail.com>
2022-08-15 23:03:23 +0700

    Java-1

---
+++ BTVN 6/Index.js
@@ -0,0 +1,20 @@
+
+// BTVN 1: Convert "Hôm nay ngày thứ 7" sang "hôm nay Ngày thứ 7"
+// let string=`Hôm nay ngày thứ 7`;
+// let string1= string.charAt(0).toLowerCase();
+// let string2= string.slice(1,7);
+// let string3= string.charAt(8).toUpperCase();
+// let string4= string.slice(9);
+// // let result = string1 + string2 + " " + string3 + string4;
+// // let result =`${string1}${string2} ${string3}${string4}`;
+// let result;
+// result=string1.concat(string2, " ", string3, string4);
+
+// console.log('Result:', result);
+
+
+// BTVN 2: Convert "Hôm nay ngày thứ 7" sang "hômnay ngày thứ 8"
+let string1=`Hôm nay ngày thứ 7`;
+let string2= string1.toLowerCase();
+let string3= string2.replace (7,8);
+console.log('Result:', string3.slice(0,3) + string3.slice(4));
