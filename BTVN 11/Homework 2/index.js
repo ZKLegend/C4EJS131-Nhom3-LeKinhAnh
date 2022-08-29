a3830ffbcc08568efac9cede916d86233250886e
ZKlegend <lekinhanh91@gmail.com>
2022-08-30 00:31:26 +0700

    BTVN 11

---
+++ BTVN 11/Homework 2/index.js
@@ -0,0 +1,9 @@
+let numbers = document.getElementById('btn');
+console.log(numbers);
+function raiseNumber () {
+    for (let i = 0; ;i++) {
+        numbers.innerHTML++;
+        return numbers.innerHTML;
+    }
+   
+}
